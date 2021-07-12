9b6988aafec6d58236c830277a8226c59d8bd649
tihomirkg91 <tihomirkg91@gmail.com>
2021-07-12 14:16:57 +0200

    v-1

--- src/script.js
+++ src/script.js
@@ -1,137 +1,137 @@
-const selectedEmailBtn = document.querySelector('.navbar__option-email');
-const selectedMobileBtn = document.querySelector('.navbar__option-mobile');
-const inputEmail = document.querySelector('.form-body__input--email');
-const inputMobile = document.querySelector('.form-body__input--mobile');
-const emailLabel = document.querySelector('.label-active__email');
-const mobileLabel = document.querySelector('.label-active__phone');
-const emailPositionBtn = document.querySelector('#navbar__radio-one');
-const form = document.getElementById('form');
-const errorMessage = document.querySelector('.render__error-msg');
-const successfulMessage = document.querySelector('.render__successful-msg');
-const backgroundBlur = document.querySelector('.form__blur');
-const spinner = document.querySelector('.form__spinner');
+const selectedEmailBtn = document.querySelector(".navbar__option-email");
+const selectedMobileBtn = document.querySelector(".navbar__option-mobile");
+const inputEmail = document.querySelector(".form-body__input--email");
+const inputMobile = document.querySelector(".form-body__input--mobile");
+const emailLabel = document.querySelector(".label-active__email");
+const mobileLabel = document.querySelector(".label-active__phone");
+const emailPositionBtn = document.querySelector("#navbar__radio-one");
+const form = document.getElementById("form");
+const errorMessage = document.querySelector(".render__error-msg");
+const successfulMessage = document.querySelector(".render__successful-msg");
+const backgroundBlur = document.querySelector(".form__blur");
+const spinner = document.querySelector(".form__spinner");
 ////////////////////////////////////////////////////////
 //Messeges
-const serverErrorMsg = 'Server is not responding! Try again latter';
-const emailErrorMsg = 'Please enter a valid e-mail address';
-const phoneErrorMsg = 'Please enter a valid phone number';
-const termsServiceMsg = 'Please accept Terms of Service';
-const accountCreateMsg = 'Your account is successfully created';
+const serverErrorMsg = "Server is not responding! Try again latter";
+const emailErrorMsg = "Please enter a valid e-mail address";
+const phoneErrorMsg = "Please enter a valid phone number";
+const termsServiceMsg = "Please accept Terms of Service";
+const accountCreateMsg = "Your account is successfully created";
 
 //Functions
 const renderFormInput = function () {
-  if (selectedEmailBtn.checked === true) {
-    inputEmail.style.display = 'block';
-    inputMobile.style.display = 'none';
-  } else {
-    inputEmail.style.display = 'none';
-    inputMobile.style.display = 'block';
-  }
+   if (selectedEmailBtn.checked === true) {
+      inputEmail.style.display = "block";
+      inputMobile.style.display = "none";
+   } else {
+      inputEmail.style.display = "none";
+      inputMobile.style.display = "block";
+   }
 };
 
 const renderDefaultLabel = function () {
-  emailPositionBtn.checked = true;
-  inputEmail.style.display = 'block';
-  inputMobile.style.display = 'none';
+   emailPositionBtn.checked = true;
+   inputEmail.style.display = "block";
+   inputMobile.style.display = "none";
 };
 
-[selectedEmailBtn, selectedMobileBtn].forEach(option =>
-  option.addEventListener('click', () => {
-    renderFormInput();
-  })
+[selectedEmailBtn, selectedMobileBtn].forEach((option) =>
+   option.addEventListener("click", () => {
+      renderFormInput();
+   })
 );
 
 const clearMessages = function () {
-  form.reset();
-  renderDefaultLabel();
-  setTimeout(() => {
-    successfulMessage.innerHTML = '';
-    errorMessage.innerHTML = '';
-  }, 3000);
+   form.reset();
+   renderDefaultLabel();
+   setTimeout(() => {
+      successfulMessage.innerHTML = "";
+      errorMessage.innerHTML = "";
+   }, 3000);
 };
 
 const renderError = function (msg) {
-  setTimeout(() => {
-    clearMessages();
-    errorMessage.innerHTML = msg;
-  }, 500);
+   setTimeout(() => {
+      clearMessages();
+      errorMessage.innerHTML = msg;
+   }, 500);
 };
 
 const renderSuccessfulMsg = function (msg) {
-  setTimeout(() => {
-    clearMessages();
-    successfulMessage.innerHTML = msg;
-  }, 500);
+   setTimeout(() => {
+      clearMessages();
+      successfulMessage.innerHTML = msg;
+   }, 500);
 };
 
 const renderSpinner = function () {
-  backgroundBlur.classList.toggle('form__blur--hidden');
-  spinner.classList.toggle('form__spinner--hidden');
+   backgroundBlur.classList.toggle("form__blur--hidden");
+   spinner.classList.toggle("form__spinner--hidden");
 };
 
 const checkServerResponse = function (data) {
-  if (data.ok === false) throw Error(serverErrorMsg);
+   if (data.ok === false) throw Error(serverErrorMsg);
 };
 
 const checkValidEmail = function (data) {
-  if (data.emailOrNumber === 'email') {
-    if (
-      data.email.indexOf('@', 0) < 0 ||
-      data.email.indexOf('.', 0) < 0 ||
-      data.email === ''
-    ) {
-      throw Error(emailErrorMsg);
-    }
-  }
+   if (data.emailOrNumber === "email") {
+      if (
+         data.email.indexOf("@", 0) < 0 ||
+         data.email.indexOf(".", 0) < 0 ||
+         data.email === ""
+      ) {
+         throw Error(emailErrorMsg);
+      }
+   }
 };
 
 const checkValidPhone = function (data) {
-  if (data.emailOrNumber === 'number') {
-    if (data.tel === '' || isNaN(data.tel)) {
-      throw Error(phoneErrorMsg);
-    }
-  }
+   if (data.emailOrNumber === "number") {
+      if (data.tel === "" || isNaN(data.tel)) {
+         throw Error(phoneErrorMsg);
+      }
+   }
 };
 
 const checkTermsConditions = function (data) {
-  if (data.termsAndConditions !== 'on') {
-    throw Error(termsServiceMsg);
-  } else {
-    renderSuccessfulMsg(accountCreateMsg);
-  }
+   if (data.termsAndConditions !== "on") {
+      throw Error(termsServiceMsg);
+   } else {
+      renderSuccessfulMsg(accountCreateMsg);
+   }
 };
 
 const initCheckErrors = function (response, data) {
-  const dataObj = Object.fromEntries(data);
-  checkServerResponse(response);
-  checkValidEmail(dataObj);
-  checkValidPhone(dataObj);
-  checkTermsConditions(dataObj);
+   const dataObj = Object.fromEntries(data);
+   checkServerResponse(response);
+   checkValidEmail(dataObj);
+   checkValidPhone(dataObj);
+   checkTermsConditions(dataObj);
 };
 
 const AJAX = async function (data) {
-  try {
-    const response = await fetch('https://formspree.io/f/xleanvle', {
-      method: 'POST',
-      body: data,
-      headers: {
-        Accept: 'application/json',
-      },
-    });
+   try {
+      const response = await fetch("https://formspree.io/f/mvodbzrq", {
+         method: "POST",
+         body: data,
+         headers: {
+            Accept: "application/json",
+         },
+      });
 
-    initCheckErrors(response, data);
-  } catch (err) {
-    renderError(err);
-  }
+      initCheckErrors(response, data);
+   } catch (err) {
+      renderError(err);
+   }
 };
 
 const handleSubmit = function (event) {
-  event.preventDefault();
-  renderSpinner();
-  setTimeout(() => {
-    renderSpinner();
-  }, 1000);
-  const data = new FormData(event.target);
-  AJAX(data);
+   event.preventDefault();
+   renderSpinner();
+   setTimeout(() => {
+      renderSpinner();
+   }, 1000);
+   const data = new FormData(event.target);
+   AJAX(data);
 };
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
